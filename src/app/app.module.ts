4a254857cbe15935105bd6c582f9eb1dd42ece23
Tomasz Nicieja <tomasznicieja@MacBook-Pro.local>
2017-01-27 21:58:54 +0100

    changing names of basic view - home page & add_entry page - reconfiguration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { HomePage } from '../pages/home/home';
+import { AddEntryPage } from '../pages/add_entry/add_entry';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    AddEntryPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +16,8 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    AddEntryPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
